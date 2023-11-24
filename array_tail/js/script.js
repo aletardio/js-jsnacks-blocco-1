d4000341aec83470cce5164fde94b3d6dba06e41
aletardio <ale.tardio99@gmail.com>
2023-11-24 17:23:01 +0100

    Added bonus for ex.2

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -2,7 +2,7 @@
 let array = [];
 
 // CHIEDERE ALL'UTENTE QUANTI ELEMENTI DOVRÀ CONTENERE L'ARRAY
-let numbers = parseInt(prompt('Quanti elementi deve contenere l/array'));
+let numbers = parseInt(prompt('Quanti elementi deve contenere l\'array'));
 
 // GENERARE TANTI NUMERI CASUALI (INTERVALLO DA 1 A 100) PER QUANTI SONO GLI ELEMENTI DA INSERIRE
 for(let i = 1; i <= numbers; i++) {
@@ -13,5 +13,15 @@ for(let i = 1; i <= numbers; i++) {
 console.log(array);
 
 // STAMPARE GLI ULTIMI 5 ELEMENTI DELL'ARRAY
-console.log(array.slice(-5).reverse());
+console.log(array.slice(-5));
 
+// BONUS
+
+let x = parseInt(prompt('Quanti elementi dell\'array dobbiamo stampare?'));
+
+if(array.length >= x) {
+    console.log('Gli ultimi ' + x + ' elementi dell\'array sono: ' + array.slice(-x))
+}
+else {
+    console.log('L\'array contiene meno di ' + x + ' elementi. Non è possibile stampare gli ultimi ' + x + ' elementi.');
+}
